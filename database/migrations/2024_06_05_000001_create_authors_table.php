3f21dc1208212dc894e677e3d7f7f91634842fda
rezaarief1411 <reza.arief@sirclo.com>
2024-06-25 20:02:52 +0700

    create additional table

---
+++ database/migrations/2024_06_05_000001_create_authors_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('authors', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',50)->comment('Authors Name');
+            $table->string('bio',255)->nullable()->comment('Authors Bio');
+            $table->timestamp('birth_date')->useCurrent()->comment('Authors Birth Date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('authors');
+    }
+};
---
+++ database/migrations/2024_06_05_000001_create_book_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title',50)->comment('Book Title');
+            $table->string('description',255)->nullable()->comment('Book Description');
+            $table->timestamp('publish_date,')->useCurrent()->comment('Book Publish Date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+};
